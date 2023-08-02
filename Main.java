93623176a13aea4dd7484d62e8d79744e9bb2d3a
Raushan Kumar <raun3220@gmail.com>
2023-08-02 10:38:21 +0530

    Bug fixed : IES-6687

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! I am learning Git and GitHub");
+        System.out.println("Hello world! I am learning Git and GitHub ");
+        System.out.println("Learning Git status");
     }
 }
